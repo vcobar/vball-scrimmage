7c9f16c2af602617c379eac58197c4b62f18bc4c
vcobar <beektor@hotmail.com>
2016-10-04 11:47:27 -0700

    add sass,bourbon and styles

--- src/player.jsx
+++ src/player.jsx
@@ -5,8 +5,6 @@ module.exports = React.createClass({
     return (
       <div className="player">
         <div className="name" key={this.props.id}>{this.props.name}</div>
-        <i className="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;
-        <i className="fa fa-minus-circle" aria-hidden="true"></i>
       </div>
     );
   }
